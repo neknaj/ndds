68c7783f7a5ab31066190fa8d44658e03b38e9d4
bem130 <bem130@ymail.ne.jp>
2024-08-04 14:49:23 +0900

    相対パスに変更

--- nml.js
+++ nml.js
@@ -52,7 +52,7 @@ var NML_Runtime = {};
 var NML_Module = {};
 
 window.addEventListener("load",async ()=>{
-    NML_Runtime = await import('/runtime/eval.js');
-    NML_Module.default = await import('/defaultmodule/default.js');
+    NML_Runtime = await import('./runtime/eval.js');
+    NML_Module.default = await import('/.defaultmodule/default.js');
     customElements.define("nml-view", NeknajMarkupLanguageElement);
 })
